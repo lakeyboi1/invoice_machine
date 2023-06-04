87f2c2a7f889d46c47718e8e2a3170a0c6cf052a
lakeyboi1 <aryan.rashid221@gmail.com>
2023-06-03 19:21:39 -0700

    rollback

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {
-  BluetoothManager,
-  BluetoothEscposPrinter,
-  BluetoothTscPrinter,
-} from "tp-react-native-bluetooth-printer";
+import { BluetoothManager, BluetoothEscposPrinter, BluetoothTscPrinter} from 'react-native-bluetooth-escpos-printer';
 
 export default function App() {
+  BluetoothManager.isBluetoothEnabled().then(
+    (enabled) => {
+      alert(enabled); // enabled ==> true /false
+      console.log(enabled);
+    },
+    (err) => {
+      alert(err);
+      console.log(err);
+    }
+  );
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
